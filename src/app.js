c70c3fccc4b44c9788b38df49f657459910c5602
emwright1 <emwright1@protonmail.com>
2022-01-23 09:45:53 +0000

    Module 3 - Wiring up transfer form

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({extended: true}));
 
 const accountData = fs.readFileSync("src/json/accounts.json", {encoding: "UTF-8"});
 const accounts = JSON.parse(accountData);
@@ -19,19 +20,43 @@ app.get("/", (req, resp) => {
 
 app.get("/savings", (req, resp) => {
     resp.render("account", {account: accounts.savings});
-})
+});
 
 app.get("/checking", (req, resp) => {
     resp.render("account", {account: accounts.checking});
-})
+});
 
 app.get("/credit", (req, resp) => {
     resp.render("account", {account: accounts.credit});
-})
+});
 
 app.get("/profile", (req, resp) => {
     resp.render("profile", {user: users[0]});
-})
+});
+
+app.get("/transfer", (req, resp) => {
+    resp.render("transfer");
+});
+
+app.post("/transfer", (req, resp) => {
+    accounts[req.body.to].balance += parseInt(req.body.amount);
+    accounts[req.body.from].balance -= parseInt(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, "json/accounts.json"), accountsJSON, "UTF-8");
+    resp.render("transfer", {message: "Transfer Completed"})
+});
+
+app.get("/payment", (req, resp) => {
+    resp.render("payment", {account: accounts.credit});
+});
+
+app.post("/payment", (req, resp) => {
+    accounts.credit.balance -= parseInt(req.body.amount);
+    accounts.credit.available += parseInt(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, "json/accounts.json"), accountsJSON, "UTF-8");
+    resp.render("payment", {message: "Payment Successful", account: accounts.credit})
+});
 
 app.listen(3000, () => {
     console.log("PS Project Running on port 3000!");
